a5aa3a38f73ddac52ecdee498e67d3badc152cd9
Sebastian Bensusan <sbensu@gmail.com>
2018-02-03 16:40:20 -0800

    copy files in their original structure

--- src/lib.rs
+++ src/lib.rs
@@ -44,41 +44,85 @@ mod cache_buster {
         }
     }
 
+    pub fn hash_and_copy(root: &Path, origin_path: &Path) {
+        // simplify this copying mess
+        let origin_buffer = origin_path.to_path_buf();
+        let origin_buffer_1 = origin_buffer.clone();
+        let origin_copy = origin_buffer_1.as_path();
+        let origin_buffer_2 = origin_buffer.clone();
+        let origin_copy_2 = origin_buffer_2.as_path();
+        if let Some(path_str) = origin_buffer.to_str() {
+            if let Some(file_name) = origin_copy.file_stem() {
+                if let Some(file_name) = file_name.to_str() {
+                    let mut file_name = file_name.to_string();
+                    let mut target_path = PathBuf::new();
+                    target_path.push(&root);
+                    match hash_file(path_str) {
+                        Ok(hash) => {
+                            file_name.push_str(".");
+                            file_name.push_str(&hash);
+                            if let Some(extension) = origin_copy.extension() {
+                                if let Some(extension) = extension.to_str() {
+                                    file_name.push_str(".");
+                                    file_name.push_str(extension);
+                                }
+                            }
+                            target_path.push(file_name);
+                        }
+                        _ => (),
+                    };
+                    if let Some(parent) = target_path.parent() {
+                        print!("{:?}", parent);
+                        match fs::create_dir_all(parent) {
+                            Ok(_) => (),
+                            Err(e) => print!("{:?}", e.to_string()),
+                        }
+                    }
+                    fs::copy(origin_copy_2, target_path);
+                }
+            };
+        }
+    }
+
+    pub fn hash_and_copy_dir(root: &Path, dir: &Path) {
+        match dir.read_dir() {
+            Ok(read_dir) => for dir_entry in read_dir {
+                match dir_entry {
+                    Ok(dir_entry) => {
+                        let path_buf = dir_entry.path();
+                        let path = path_buf.as_path();
+                        if let Some(new_parent) = path.parent() {
+                            let root_buf = root.join(new_parent);
+                            let new_root = root_buf.as_path();
+                            if path.is_dir() {
+                                hash_and_copy_dir(&new_root, path);
+                            } else {
+                                hash_and_copy(&new_root, path);
+                            }
+                        }
+                    }
+                    _ => (),
+                }
+            },
+            _ => (),
+        }
+    }
+
     pub fn list_dir() {
         let root = Path::new("target");
         for entry in glob("examples/*").expect("Failed to read glob pattern") {
             match entry {
                 Ok(origin_path) => {
-                    let origin_buffer = origin_path.to_path_buf();
-                    let origin_buffer_1 = origin_buffer.clone();
-                    let origin_copy = origin_buffer_1.as_path();
-                    let origin_buffer_2 = origin_buffer.clone();
-                    let origin_copy_2 = origin_buffer_2.as_path();
-                    if let Some(path_str) = origin_buffer.to_str() {
-                        if let Some(file_name) = origin_copy.file_stem() {
-                            if let Some(file_name) = file_name.to_str() {
-                                let mut file_name = file_name.to_string();
-                                let mut target_path = PathBuf::new();
-                                target_path.push(&root);
-                                match hash_file(path_str) {
-                                    Ok(hash) => {
-                                        file_name.push_str(".");
-                                        file_name.push_str(&hash);
-                                        if let Some(extension) = origin_copy.extension() {
-                                            if let Some(extension) = extension.to_str() {
-                                                file_name.push_str(".");
-                                                file_name.push_str(extension);
-                                            }
-                                        }
-                                        target_path.push(file_name);
-                                    }
-                                    _ => (),
-                                };
-                                fs::copy(origin_copy_2, target_path);
-                            }
-                        };
+                    if origin_path.is_dir() {
+                        // recur and do whatever you were going to do for a file
+                        hash_and_copy_dir(root, &origin_path);
+                    } else {
+                        if let Some(new_parent) = origin_path.parent() {
+                            let root_buf = root.join(new_parent);
+                            let new_root = root_buf.as_path();
+                            hash_and_copy(new_root, &origin_path);
+                        }
                     }
-                    ()
                 }
                 Err(e) => println!("{:?}", e),
             }
@@ -89,7 +133,6 @@ mod cache_buster {
 #[cfg(test)]
 mod tests {
     use cache_buster;
-    use std::result::Result;
 
     #[test]
     fn find_md5_hash() {
